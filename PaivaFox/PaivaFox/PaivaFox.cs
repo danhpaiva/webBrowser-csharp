f241444843671f1651a7755ab427b0d3baf7b145
Daniel Paiva <danpaiva@live.com>
2020-09-17 17:49:30 -0300

    Adicionado botões

--- PaivaFox/PaivaFox/PaivaFox.cs
+++ PaivaFox/PaivaFox/PaivaFox.cs
@@ -16,5 +16,26 @@ namespace PaivaFox
         {
             InitializeComponent();
         }
+
+        private void Navegar()
+        {
+            if (tb_url.Text != "")
+                webBrowser1.Navigate(tb_url.Text);
+            else
+            {
+                MessageBox.Show("Digite um endereço válido");
+                tb_url.Focus();
+            }
+        }
+
+        private void Btn_ir_Click(object sender, EventArgs e)
+        {
+            Navegar();
+        }
+
+        private void Tb_url_KeyDown(object sender, KeyEventArgs e)
+        {
+            Navegar();
+        }
     }
 }
